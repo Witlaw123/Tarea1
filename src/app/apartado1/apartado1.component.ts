69411d87808211c37194090eb9f10d900808b9cd
Pau Ernesto Sole Contreras <p.e.sole.c@live.com>
2020-05-22 20:44:03 -0600

    Hasta aqui jala con constructor 1

--- src/app/apartado1/apartado1.component.ts
+++ src/app/apartado1/apartado1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Apartado1} from './apartado1'
 
 @Component({
   selector: 'app-apartado1',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./apartado1.component.css']
 })
 export class Apartado1Component implements OnInit {
+
+
+  apartado1 : any=[
+    new Apartado1 (1, 'Juanito'),
+    new Apartado1 (2, 'Pepito'),
+    new Apartado1 (3, 'Fulanito'),
+  ]
+
 
   constructor() { }
 
